1ef28c1875743415a0d77035670c14bc3f640a5a
cheoljundev <cheoljundev@gmail.com>
2024-07-12 16:37:28 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/SpringJpaApplication1Application.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaApplication1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaApplication1Application.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/jpashop/SpringJpaApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplication1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
