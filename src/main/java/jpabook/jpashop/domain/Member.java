09433467034c53a9a220c5f5fde38a8ba806b4c5
cheoljundev <cheoljundev@gmail.com>
2024-07-13 13:16:41 +0900

    회원 기능 테스트

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
