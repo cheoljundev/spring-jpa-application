118a2f85ca5f836d21af0b3b5ae8fa1406355787
cheoljundev <cheoljundev@gmail.com>
2024-07-21 23:22:31 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출 : 많은 문제가 발생함

--- src/main/java/jpabook/jpashop/SpringJpaApplication1Application.java
+++ src/main/java/jpabook/jpashop/SpringJpaApplication1Application.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Version;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringJpaApplication1Application {
