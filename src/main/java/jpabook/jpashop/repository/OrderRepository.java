c6e619f04be3b9090611dbc551c0fed543de1094
cheoljundev <cheoljundev@gmail.com>
2024-07-22 16:14:47 +0900

    간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.order.simplequery.SimpleOrderQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -100,4 +101,12 @@ public class OrderRepository {
                         "join fetch o.delivery d", Order.class
         ).getResultList();
     }
+
+//    public List<SimpleOrderQueryDto> findOrderDtos() {
+//        return em.createQuery(
+//                "select  new jpabook.jpashop.repository.order.simplequery.SimpleOrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o " +
+//                        "join o.member m " +
+//                        "join o.delivery d", SimpleOrderQueryDto.class
+//        ).getResultList();
+//    }
 }
