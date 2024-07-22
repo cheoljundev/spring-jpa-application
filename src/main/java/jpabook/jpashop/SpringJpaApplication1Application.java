1833ff9465ef20d6c11b1e5c1382838db82e6348
cheoljundev <cheoljundev@gmail.com>
2024-07-22 17:38:53 +0900

    주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/SpringJpaApplication1Application.java
+++ src/main/java/jpabook/jpashop/SpringJpaApplication1Application.java
@@ -1,13 +1,21 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringJpaApplication1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJpaApplication1Application.class, args);
+    }
+
+    @Bean
+    Hibernate5JakartaModule hibernate5JakartaModule() {
+        return new Hibernate5JakartaModule();
     }
 
 }
